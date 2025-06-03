95bcc126149f8d20d94b0c71d90242f647ba45d1
Mithil Hassan <mithilhassan007@gmail.com>
2025-06-03 20:35:05 +0600

    initial commit

---
+++ models/User.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { unique } from "next/dist/build/utils";
+
+const userSchema = new mongoose.Schema({
+    _id: {type: string, required: true},
+    name: {type: string, required: true},
+    email: {type: string, required: true, unique: true},
+    imageUrl: {type: string, required: true},
+    cartItems: {type: Object, default: {}},
+
+}, {minimize: false})
+
+const User = mongoose.models.user || mongoose.model('user', userSchema)
+
+export default User
